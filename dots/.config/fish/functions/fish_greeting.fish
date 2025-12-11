ac9ddd2b728dedc153af967ac8699a9f9c4c2d34
pabcihba <pabcihba@gmail.com>
2025-12-11 18:15:23 +0300

    dotfiles arrival

--- dots/.config/fish/functions/fish_greeting.fish
+++ dots/.config/fish/functions/fish_greeting.fish
@@ -1,1 +1,11 @@
-fastfetch --config arch
+#!/usr/bin/fish
+
+set -l commands \
+    "fastfetch --config arch -l .local/share/fastfetch/ascii/star.txt" \
+    "fastfetch --config arch -l .local/share/fastfetch/ascii/star2.txt" \
+    "fastfetch --config arch -l .local/share/fastfetch/ascii/star3.txt --logo-padding-top 3" \
+    "fastfetch --config arch -l .local/share/fastfetch/ascii/star4.txt --logo-padding-top 1" \
+    "fastfetch --config arch -l .local/share/fastfetch/ascii/star6.txt --logo-padding-top 2"
+
+    set -l random_command $commands[(random 1 (count $commands))]
+eval $random_command
--- dots/.config/hypr/scripts/song_detail.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-song_info=$(playerctl metadata --format '    {{title}} - {{artist}}')
-
-echo "$song_info"
-
