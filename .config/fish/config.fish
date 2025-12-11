a73590a2bc541bd5e5bd55b6376141af6e07562f
pabcihba <pabcihba@gmail.com>
2025-12-11 21:26:12 +0300

    test update

--- .config/fish/config.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    zoxide init fish | source
-    alias cd z
-end
--- .config/fish/functions/entervenv.fish
+++
@@ -1,3 +0,0 @@
-function entervenv --wraps='source /home/painvision/venv/bin/activate.fish' --description 'alias entervenv source /home/painvision/venv/bin/activate.fish'
-    source /home/painvision/venv/bin/activate.fish $argv
-end
