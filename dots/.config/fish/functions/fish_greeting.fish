8197bf4a8b8513fdc8f300b7e1bd15cfd5a5f8cc
pabcihba <pabcihba@gmail.com>
2025-12-11 22:12:40 +0300

    update

--- dots/.config/fish/functions/fish_greeting.fish
+++ dots/.config/fish/functions/fish_greeting.fish
@@ -2,10 +2,7 @@
 
 set -l commands \
     "fastfetch --config arch -l .local/share/fastfetch/ascii/star.txt" \
-    "fastfetch --config arch -l .local/share/fastfetch/ascii/star2.txt" \
-    "fastfetch --config arch -l .local/share/fastfetch/ascii/star3.txt --logo-padding-top 3" \
-    "fastfetch --config arch -l .local/share/fastfetch/ascii/star4.txt --logo-padding-top 1" \
-    "fastfetch --config arch -l .local/share/fastfetch/ascii/star6.txt --logo-padding-top 2"
+    "fastfetch --config arch -l .local/share/fastfetch/ascii/star2.txt"
 
     set -l random_command $commands[(random 1 (count $commands))]
 eval $random_command
